75421324f09a8b43de2a094e7e9656a8199a87ab
Dalton Barreto <daltonmatos@gmail.com>
2020-03-05 21:57:11 -0300

    Ajustando `/apps/*` para não receber Request

--- asgard/api/apps.py
+++ asgard/api/apps.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 from asyncworker import RouteTypes
+from asyncworker.http.decorators import parse_path
 
 from asgard.api.resources.apps import AppStatsResource
 from asgard.app import app
@@ -11,11 +12,9 @@ from asgard.models.user import User
 from asgard.services.apps import AppsService
 
 
-async def get_app_stats(request: web.Request, interval: Interval):
-    app_id: str = request.match_info["app_id"]
-    user = await User.from_alchemy_obj(request["user"])
-
-    account = await Account.from_alchemy_obj(request["user"].current_account)
+async def get_app_stats(
+    app_id: str, user: User, account: Account, interval: Interval
+):
     stats = await AppsService.get_app_stats(
         app_id, interval, user, account, mesos
     )
@@ -27,8 +26,9 @@ async def get_app_stats(request: web.Request, interval: Interval):
     ["/apps/{app_id:[.a-z0-9/-]+}/stats"], type=RouteTypes.HTTP, methods=["GET"]
 )
 @auth_required
-async def app_stats(request: web.Request):
-    return await get_app_stats(request, Interval.ONE_HOUR)
+@parse_path
+async def app_stats(app_id: str, user: User, account: Account):
+    return await get_app_stats(app_id, user, account, Interval.ONE_HOUR)
 
 
 @app.route(
@@ -37,5 +37,6 @@ async def app_stats(request: web.Request):
     methods=["GET"],
 )
 @auth_required
-async def app_stats_avg_1min(request: web.Request):
-    return await get_app_stats(request, Interval.ONE_MINUTE)
+@parse_path
+async def app_stats_avg_1min(app_id: str, user: User, account: Account):
+    return await get_app_stats(app_id, user, account, Interval.ONE_MINUTE)
